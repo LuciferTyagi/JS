3ab426953610a8294e6df67105a202e0c0d0322b
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-05-22 20:57:28 +0530

    ConversionOperation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33";
+
+// console.log(typeof score);
+
+let valInNum = Number(score);
+console.log(valInNum);
+console.log(typeof valInNum);
+
+let bool = 1;
+
+console.log(typeof bool);
+
+let boolConvert =  Boolean(bool);
+console.log(boolConvert)
