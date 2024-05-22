248a7f5a672e1479b90f6381e388a095120c2bd9
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-05-22 20:40:12 +0530

    dataTypes

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let accountEmail = "yagyanshtyagi61@gmail.com";
+var accountPassword = "12345";
+let accountState;
+accountCity = "jaipur";
+
+accountEmail = "tyagi@gmail.com";
+accountPassword = "8265";
+accountCity = "hapur";
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountState]);
---
+++ 02_dataTypes.js
@@ -0,0 +1,8 @@
+//number => (2 to 2^53)
+//string
+//boolean
+//bigint
+//null
+//undefined
+//object
+//symbol
